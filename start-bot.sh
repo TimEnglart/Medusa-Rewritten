984fab3bc8dcffd5e4c3d0e0f53b65a6ecf57f67
Tim Englart <timenglart@gmail.com>
2019-08-12 23:46:06 +1000

    update config 2

--- start-bot.sh
+++ start-bot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 REPO_DIR=/srv/Medusa-Rewritten
 cd $REPO_DIR
-git fetch
+git fetch --all
 gitdiff=$(git diff master origin/master)
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]
 then
-git pull origin master
+git reset --hard origin/master
 fi
 npm run build
 node "./lib/index.js"
