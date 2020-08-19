4c277d635410083d9c6dd47c4f98c5c3ef4efec8
Tim Englart <timenglart@gmail.com>
2020-08-19 18:36:27 +1000

    remove github updater in shell

--- start-bot.sh
+++ start-bot.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 # Allow SSH Connection to Github For Private Repo
-eval $(ssh-agent);
-ssh-add ~/.ssh/github_rsa;
+# Too much of a Hassle When Running under a service
+# eval $(ssh-agent);
+# ssh-add ~/.ssh/github_rsa;
 
 # Set CWD
 REPO_DIR=/srv/Medusa-Rewritten;
 cd $REPO_DIR;
 
 # Get Lastest Github Commit
-git fetch --all
-gitdiff=$(git diff master origin/master)
-if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]
-then
-git reset --hard origin/master
-chmod -R a+rX *;
-chmod +x "./start-bot.sh"; # Make Updated Script Execuatble
-npm i;
-fi
+# Too much of a Hassle When Running under a service
+
+# git fetch --all
+# gitdiff=$(git diff master origin/master)
+# if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]
+# then
+# git reset --hard origin/master
+# chmod -R a+rX *;
+# chmod +x "./start-bot.sh"; # Make Updated Script Execuatble
+# npm i;
+# fi
 
 # Get Vue Sorted
 cd './client';
